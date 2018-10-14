8f71d27155705365433ce1d885a4e7adab15a34f
Stephen Brown <steve@evolvedlight.co.uk>
2018-10-14 23:07:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace samplesay_local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
